c5b0947a7971d9937186efec70336adff1a85632
Syd Balbidas <rasydbalbidas@gmail.com>
2018-06-23 19:38:08 +0800

    Update login and index and added COA form

--- ra-idlis/resources/views/client/index.blade.php
+++ ra-idlis/resources/views/client/index.blade.php
@@ -124,7 +124,7 @@
               </div>
             </div>
           </div>
-          <div class="card my-4 introjs-showElement introjs-relativePosition"  data-intro="Announcements<br><small>Here is where new Announcements can be found.</small>" data-step="3" >
+          <div class="card my-4 introjs-showElement introjs-relativePosition">
             <p class="card-header"><small>Click for our <i class="fa fa-hand-o-right"></i>&nbsp;<a href="">Definition of Terms</a></small></p>
           </div>
           <div class="text-center introjs-showElement introjs-relativePosition" data-intro="Hello world! I'm Intro.js" data-step="8">
