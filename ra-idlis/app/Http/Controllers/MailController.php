910f5fbc725e2ca6d467f2b0e6af7e7dd13f8295
Syd Balbidas <rasydbalbidas@gmail.com>
2018-06-30 16:21:14 +0800

    fixed email spam

--- ra-idlis/app/Http/Controllers/MailController.php
+++ ra-idlis/app/Http/Controllers/MailController.php
@@ -5,26 +5,90 @@
 
 	use App\Http\Requests;
 	use App\Http\Controllers\Controller;
-
+   use Carbon\Carbon;
+   use Illuminate\Support\Str;
+   use Hash;
+   use DB;
+      
 	class MailController extends Controller
 	{
-		 public function basic_email(){
-      $data = array('name'=>"Virat Gandhi", 'email'=>'ra.reancyvillacarlos@gmail.com');
-      $email='ra.reancyvillacarlos@gmail.com';
-      Mail::send(['text'=>'mail'], $data, function($message) {
-         $message->to($email, 'Reancy Villacarlos')->subject
-            ('Laravel Basic Testing Mail');
-         $message->from('rasydbalbidas@gmail.com','Virat Gandhi');
-      });
-      echo "Basic Email Sent. Check your inbox.";
-   }
-   public function html_email(){
-      $data = array('name'=>"Virat Gandhi");
-      Mail::send('mail', $data, function($message) {
-         $message->to('sydpalacio@gmail.com', 'Tutorials Point')->subject
-            ('Laravel HTML Testing Mail');
-         $message->from('rasydbalbidas@gmail.com','Virat Gandhi');
-      });
+   public function auto_mailer(Request $request){
+      if($request->isMethod('get')){
+         return view('client.register');
+      }
+      if($request->isMethod('post')){
+          $dt = Carbon::now();
+          $dateNow = $dt->toDateString();
+          $timeNow = $dt->toTimeString();
+          $data['facility_name'] = $request->facility_name;
+          $data['region'] = $request->region;
+          $data['province'] = $request->province;
+          $data['brgy'] = $request->brgy;
+          $data['street'] = $request->street;
+          $data['city_muni'] = $request->city_muni;
+          $data['zip'] = $request->zipcode;
+          $data['authorized'] = $request->auth_name;
+          $data['tel'] = $request->tel;
+          $data['cel'] = $request->cel;
+          $data['uname'] = strtoupper($request->uname);
+          $data['pass'] = Hash::make($request->pass2);
+          $data['email'] = $request->email;
+          $data['contact_p'] = $request->contact_p;
+          $data['contact_pno'] = $request->contact_pno;
+          $data['ip'] = request()->ip();
+          $data['token'] = Str::random(40);
+          // Check Username
+          $checkUser = DB::table('x08')
+                        ->where('uid', '=' ,$data['uname'])
+                        ->where('grpid' , '=' , 'C')
+                        ->exists();
+          // Check Facility Name
+          $checkFacility = DB::table('x08')
+                        ->where('facilityname', '=' , $data['facility_name'])
+                        ->exists();
+          if ($checkUser == true) {
+              return 'same';
+          } else if ($checkFacility == true) {
+              return 'sameFacility';
+          }
+          else {
+            $data1 = array('name'=>"DOH Support", 'token'=>$request->token);
+            Mail::send('mail', $data1, function($message) use ($request) {
+               $message->to($request->email, $request->facility_name)->subject
+                  ('Verify Email Account');
+               $message->from('doholrs@gmail.com','DOH Support');
+            });
+
+            DB::table('x08')->insert(
+                [
+                    'uid' => $data['uname'],
+                    'pwd' => $data['pass'],
+                    'facilityname' => $data['facility_name'],
+                    // 'rgnid_address' => $data['regionadd'],
+                    'rgnid' => $data['region'],
+                    'province' => $data['province'],
+                    'barangay' => $data['brgy'],
+                    'streetname' => $data['street'],
+                    'city_muni' => $data['city_muni'],
+                    'zipcode' => $data['zip'],
+                    'contactperson' => $data['contact_p'],
+                    'contactpersonno' => $data['contact_pno'],
+                    'rgnid_address' => $data['tel'],
+                    'email' => $data['email'],
+                    'authorizedsignature' => $data['authorized'],
+                    'contact' => $data['cel'],
+                    'ipaddress' => $data['ip'],
+                    't_date' => $dateNow,
+                    't_time' =>$timeNow,
+                    'grpid' => 'C',
+                    'token' => $data['token']
+                ]
+            );
+            // return response()->json(['test'=>$data]);
+            // return "Check";
+            return 'DONE';
+          }
+        }  
       echo "HTML Email Sent. Check your inbox.";
    }
    public function attachment_email(){
@@ -37,4 +101,16 @@
       });
       echo "Email Sent with attachment. Check your inbox.";
    }
+
+    public function resend_ver(Request $req, $id) {
+      $data = DB::table('x08')->where('uid', '=', $id)->first();
+      $data1 = array('name'=>$data->facilityname, 'token'=>$data->token);
+   
+      Mail::send('mail', $data1, function($message) use ($data) {
+         $message->to($data->email, $data->facilityname)->subject
+            ('Verify your Account in DOH OLRS');
+         $message->from('doholrs@gmail.com', 'DOH OLRS Support');
+      });
+      return redirect()->route('client');
+    }      
 }
--- ra-idlis/resources/views/mail.blade.php
+++ ra-idlis/resources/views/mail.blade.php
@@ -1,3 +1,8 @@
-<h1>Hi, {{ $name }}</h1>
+<center><h1>DOHOLRS Account Registration</h1></center>
+<h3>Hi, {{ $name }}</h3>
+<p>Thank you for registering in our website. But, you must first verify your account</p>
 <p>Verify your account by clicking the link below</p>
-<a href="{{ asset('/register/verify') }}/{{ $token }}">Verify account</a>
+<a href="{{ asset('/register/verify') }}/{{ $token }}"><button>Verify account</button></a>
+<p disabled>Note: <strong>Unverified Accounts</strong> are restricted from logging in</p>
+<p>Regards,</p>
+<p>DOH Support</p>
