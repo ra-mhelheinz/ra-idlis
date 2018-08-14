1ee0d153d515afefb8a819e64e2d32dc7c8077fe
Syd Balbidas <rasydbalbidas@gmail.com>
2018-08-14 17:29:52 +0800

    Updated orderofpayment

--- ra-idlis/resources/views/client/evaluate.blade.php
+++ ra-idlis/resources/views/client/evaluate.blade.php
@@ -1,24 +1,34 @@
 @extends('main')
 @section('content')
 @include('client.nav')
+@if (session()->exists('client_data') || session('client_data') != null)
+   @php
+     $clientData = session('client_data');
+   @endphp
+@else
+  <script type="text/javascript">
+    window.location.href = "{{asset('/')}}";
+  </script>
+@endif
+
 @include('client.breadcrumb')
 
 <script type="text/javascript">
 	document.getElementById('third').style = "color: blue;";
 </script>
 <div class="jumbotron container" style="margin-top: 2%;border: 1px solid rgba(0,0,0,.2);margin-top: 2%;;background-color: #fff">
-	<div class="text-center"><h3>Your Application is not yet evaluated.</h3></div>
-		<div class="container" hidden>
+	<div class="text-center" hidden><h3>Your Application is not yet evaluated.</h3></div>
+		<div class="container" >
 			<div class="table-responsive">
-				<table class="table table-borderless">
+				<table class="table" style="width: 100%;">
 					<thead>
 						<tr>
-							<td width="80%">
-								<h2>ABC Hospital</h2>
-								<h4>Rizal St. Manila</h4>
+							<td style="width: 50%;">
+								<h4>{{$clientData->facilityname}},{{$clientData->streetname}}</h4>
+
 							</td>
-							<td>
-								<center><h4>DOH Evaluation Status</h4></center>
+							<td style="width: 50%;" class="text-center">
+								<h4>DOH Evaluation Status</h4>
 							</td>
 						</tr>
 					</thead>
@@ -34,154 +44,16 @@
 									<i class="fa fa-times" style="color: red;"></i>
 								</center>
 							</td>
-							<td>
-								<center>
-									<input type="file" name="">
-								</center>
-							</td>
-						</tr>
-						<!-- //// -->
-						<tr>
-							<td >
-								<font>
-									2. List of Personnel
-								</font>
-							</td>
-							<td>
-								<center>
-									<i class="fa fa-times" style="color: red;"></i>
-								</center>
-							</td>
-							<td>
-								<center>
-									<input type="file" name="">
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									3. List of Equipment/Instrument
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									4. List of Ancillary Services
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									5. Application Form (for Medical X-ray)
-								</font>
-							</td>
-							<td>
-								<center>
-									<i class="fa fa-times" style="color: red;"></i>
-								</center>
-							</td>
-							<td>
-								<center>
-									<input type="file" name="">
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									6. Application Form (Hospital Pharmacy)
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									7. Geographic Form (Location Map)
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									8. Photographs of exterior & interior of facility
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
-						</tr>
-						<!-- /// -->
-						<tr>
-							<td >
-								<font>
-									9. Annual Statistical Reports
-								</font>
-							</td>
-							<td>
-								<center>
-									
-									<button type="button" class="btn btn-success">
-										<i class="fa fa-check" aria-hidden="true"></i>
-									</button>
-								</center>
-							</td>
 						</tr>
 					</tbody>
 				</table>
 			</div>
 			<hr>
-				<div class="row">
+				<div class="container row">
 					<div class="col-sm-6" >
 							<label>Recommended for Inspection:</label>
 							<!-- data-toggle="modal" data-target="#exampleModalCenter" -->
-							<strong><label style="color:green;">&nbsp;Yes</label></strong>
-							<a href="{{asset('client/orderofpaymentc')}}" style="text-decoration: none;">View payment</a>
+							<strong><label style="color:green;">&nbsp;</label></strong>
 					</div>	
 					<div class="col-sm-6">
 						<span>
@@ -192,7 +64,7 @@
 				</div>
 				<div class="row">
 					<div class="col-sm-4"></div>
-					<div class="col-sm-4 text-center"><button class="btn-primarys">Re-Submit</button></div>
+					<div class="col-sm-4 text-center"><button class="btn-primarys"></button></div>
 					<div class="col-sm-4"></div>
 				</div>
 		</div>	
